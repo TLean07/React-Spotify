f63a61a14fb83cd2825fa48c2127a2a619af613d
Leandro <125459372+TLean07@users.noreply.github.com>
2024-09-11 09:42:57 -0300

    commit

---
+++ src/components/Countainer.jsx
@@ -0,0 +1,7 @@
+export default function Cointainer({ children }){
+    return(
+        <section className="w-full h-dvh flex">
+            {children}
+        </section>
+    ) 
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+export default function SideBar({ children }) {
+    return(
+        <aside className="bg-green-400 w-1/4 flex flex-col justify-around items-center">
+        <div className="bg-red-500 w-32 h-32"></div>
+        <div className="bg-red-500 w-32 h-32"></div>
+        <div className="bg-red-500 w-32 h-32"></div>  
+        {children}
+        </aside>
+    )
+}
